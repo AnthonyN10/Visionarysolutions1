be7d8b23b9b2f2e1d5b7f8efe461b2d02f3d6ad5
AnthonyN10 <127478801+AnthonyN10@users.noreply.github.com>
2025-04-19 08:21:09 +0200

    2

--- src/components/ServiceCard.tsx
+++ src/components/ServiceCard.tsx
@@ -62,7 +62,7 @@ export const ServiceCard = ({ title, image, detailedInfo }: ServiceCardProps) =>
                   setIsFlipped(false);
                 }}
               >
-                Back to Front
+                BACK TO FRONT
               </button>
             </div>
           </div>
